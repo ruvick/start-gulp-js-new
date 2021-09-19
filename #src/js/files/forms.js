20d551151bf6b725195c8d8f4e0c51c9fbd72abb
Ruvick <rudikov.web@ya.ru>
2021-09-19 19:58:55 +0300

    Форма с инпутами, маска, валидация, фокус

--- #src/js/files/forms.js
+++ #src/js/files/forms.js
@@ -391,7 +391,7 @@ function inputs_init(inputs) {
 					//'+38(999) 999 9999'
 					//'+375(99)999-99-99'
 					input.classList.add('_mask');
-					Inputmask("+375 (99) 9999999", {
+					Inputmask("+7(999) 999 9999", {
 						//"placeholder": '',
 						clearIncomplete: true,
 						clearMaskOnLostFocus: true,
