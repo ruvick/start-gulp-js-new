759033f78c1c2629d24c941c53f0ad37a1051f6f
ruvick <rudikov.web@ya.ru>
2023-11-23 08:56:35 +0300

    Заменил стартовые файлы

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -11,55 +11,55 @@ const headsearch = document.querySelector(".header__search-mob");
 
 //BURGER
 if (iconMenu) {
-  iconMenu.addEventListener("click", function () {
-    iconMenu.classList.toggle("active");
-    body.classList.toggle("_lock");
-    menuBody.classList.toggle("active");
-  });
+	iconMenu.addEventListener("click", function () {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("_lock");
+		menuBody.classList.toggle("active");
+	});
 }
 
 // Закрытие моб меню при клике на якорную ссылку
 if (menuListItemElems) {
-  menuListItemElems.addEventListener("click", function () {
-    iconMenu.classList.toggle("active");
-    body.classList.toggle("_lock");
-    menuBody.classList.toggle("active");
-  });
+	menuListItemElems.addEventListener("click", function () {
+		iconMenu.classList.toggle("active");
+		body.classList.toggle("_lock");
+		menuBody.classList.toggle("active");
+	});
 }
 
 // Строка поиска на мобилках 
 if (mobsearch) {
-  mobsearch.addEventListener("click", function () {
-    headsearch.classList.toggle("_active");
-  });
+	mobsearch.addEventListener("click", function () {
+		headsearch.classList.toggle("_active");
+	});
 }
 
 // Закрытие моб меню при клике вне области меню 
 window.addEventListener('click', e => { // при клике в любом месте окна браузера
-  const target = e.target // находим элемент, на котором был клик
-  if (!target.closest('.icon-menu') && !target.closest('.mob-menu') && !target.closest('.header__mob-search-btn') && !target.closest('.header__search-mob') && !target.closest('._popup-link') && !target.closest('.popup')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-    iconMenu.classList.remove('active') // то закрываем окно навигации, удаляя активный класс
-    menuBody.classList.remove('active')
-    body.classList.remove('_lock')
-    headsearch.classList.remove('_active')
-  }
+	const target = e.target // находим элемент, на котором был клик
+	if (!target.closest('.icon-menu') && !target.closest('.mob-menu') && !target.closest('.header__mob-search-btn') && !target.closest('.header__search-mob') && !target.closest('._popup-link') && !target.closest('.popup')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+		iconMenu.classList.remove('active') // то закрываем окно навигации, удаляя активный класс
+		menuBody.classList.remove('active')
+		body.classList.remove('_lock')
+		headsearch.classList.remove('_active')
+	}
 })
 
 // Плавная прокрутка
 const smotScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
 
 smotScrollElems.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault()
-    console.log(event);
+	link.addEventListener('click', (event) => {
+		event.preventDefault()
+		console.log(event);
 
-    const id = link.getAttribute('href').substring(1)
-    console.log('id : ', id);
+		const id = link.getAttribute('href').substring(1)
+		console.log('id : ', id);
 
-    document.getElementById(id).scrollIntoView({
-      behavior: 'smooth'
-    });
-  })
+		document.getElementById(id).scrollIntoView({
+			behavior: 'smooth'
+		});
+	})
 });
 
 
@@ -68,8 +68,8 @@ const scrollProgress = document.getElementById('scroll-progress');
 const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
 window.addEventListener('scroll', () => {
-  const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-  scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+	const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+	scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
 });
 
 
@@ -77,14 +77,14 @@ window.addEventListener('scroll', () => {
 const priceEl = document.querySelector(".price");
 
 function changePrice(price) {
-  priceEl.innerText = price;
-  console.log(price);
+	priceEl.innerText = price;
+	console.log(price);
 };
 
 
 // Подсказки
 tippy('._tippy', {
-  content: "Подсказка",
+	content: "Подсказка",
 });
 
 
@@ -94,28 +94,28 @@ const BarLinkIconElems = document.querySelectorAll('.sidebar__menu-icon');
 const BarSubMenuElems = document.querySelectorAll('.sidebar__submenu');
 
 BarIconElems.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
+	btn.addEventListener('click', () => {
 
-    if (!btn.classList.contains('sidebar__menu-icon_active')) {
+		if (!btn.classList.contains('sidebar__menu-icon_active')) {
 
-      BarSubMenuElems.forEach((BarSubMenuElem) => {
-        BarSubMenuElem.classList.remove('active')
-      });
-      BarIconElems.forEach((BarIconElem) => {
-        BarIconElem.classList.remove('sidebar__menu-icon_active')
-      });
-      BarLinkIconElems.forEach((BarLinkIconElem) => {
-        BarLinkIconElem.classList.remove('sidebar__menu-icon_active')
-      });
+			BarSubMenuElems.forEach((BarSubMenuElem) => {
+				BarSubMenuElem.classList.remove('active')
+			});
+			BarIconElems.forEach((BarIconElem) => {
+				BarIconElem.classList.remove('sidebar__menu-icon_active')
+			});
+			BarLinkIconElems.forEach((BarLinkIconElem) => {
+				BarLinkIconElem.classList.remove('sidebar__menu-icon_active')
+			});
 
-      BarSubMenuElems[index].classList.add('active')
-      BarLinkIconElems[index].classList.add('sidebar__menu-icon_active')
-      btn.classList.add('sidebar__menu-icon_active')
-    } else {
-      BarSubMenuElems[index].classList.remove('active')
-      BarLinkIconElems[index].classList.remove('sidebar__menu-icon_active')
-      btn.classList.remove('sidebar__menu-icon_active')
-    }
-  })
+			BarSubMenuElems[index].classList.add('active')
+			BarLinkIconElems[index].classList.add('sidebar__menu-icon_active')
+			btn.classList.add('sidebar__menu-icon_active')
+		} else {
+			BarSubMenuElems[index].classList.remove('active')
+			BarLinkIconElems[index].classList.remove('sidebar__menu-icon_active')
+			btn.classList.remove('sidebar__menu-icon_active')
+		}
+	})
 })
 
