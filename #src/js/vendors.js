8a6fe5edfc2a7995f35e880fd9c9c95f31ebc061
Ruvick <rudikov.web@ya.ru>
2023-11-25 20:49:15 +0300

    Revert "Доп весртка"
    This reverts commit 062171b4ee003d8d85163260cded5526d1c4e5b9.

--- #src/js/vendors.js
+++ #src/js/vendors.js
@@ -1,4 +1,4 @@
-// @@include('libs/smoothScroll.js', {})
+@@include('libs/smoothScroll.js', {})
 @@include('libs/inputmask.min.js', {})
 @@include('libs/swiper.min.js', {})
 @@include('libs/nouislider.js', {})
